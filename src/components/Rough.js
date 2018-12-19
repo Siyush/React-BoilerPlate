c8529f302eb6df058d6973c208c2b1ea7177b0eb
Sushilkumar Shinde <sushilkumarshinde06@gmail.com>
2018-12-19 17:51:48 +0530

    React Boiler Plate

---
+++ src/components/Rough.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Rough() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+import React, { Component } from 'react'
+
+class Rough extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default  Rough;
